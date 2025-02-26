a6516f9b29cc91b5d17ff558a461f1be34d5eba2
KevinTheRainmaker <sweetrainforyou@gmail.com>
2025-02-26 13:09:28 +0900

    feat: training method

--- multi-gpu-full/1_train_full_fine_tuning.py
+++ multi-gpu-full/1_train_full_fine_tuning.py
@@ -28,6 +28,13 @@ login(
     add_to_git_credential=True
 )
 
+
+@dataclass
+class ScriptArguments:
+    dataset_path: str = field(default=None, metadata={'help': 'Path to dataset.'})
+    model_name: str = field(default=None, metadata={'help':'Model name for SFT Training.'})
+    max_seq_length: int = field(default=None, metadata={'help':'Max Sequence Length for SFT Trainer'})
+
 dataset = load_dataset("ccw7463/Ko_ARC_ver0.3")
 # {'instruction': Problem statement, 'input': Options, 'output': Answer}
 
@@ -70,3 +77,76 @@ train_dataset = train_dataset['train'].train_test_split(test_size=0.1, seed=42)
 
 train_dataset['train'].to_json('multi-gpu-full/dataset/Ko_ARC_ver0.3/train_dataset.json', orient='records', force_ascii=False)
 train_dataset['test'].to_json('multi-gpu-full/dataset/Ko_ARC_ver0.3/test_dataset.json', orient='records', force_ascii=False)
+
+LLAMA_3_CHAT_TEMPLATE = (
+    "{% for message in messages %}"
+        "{% if message['role'] == 'system' %}"
+            "{{ message['content'] }}"
+        "{% elif message['role'] == 'user' %}"
+            "{{ '\n\nHuman: ' + message['content'] +  eos_token }}"
+        "{% elif message['role'] == 'assistant' %}"
+            "{{ '\n\nAssistant: '  + message['content'] +  eos_token  }}"
+        "{% endif %}"
+    "{% endfor %}"
+    "{% if add_generation_prompt %}"
+    "{{ '\n\nAssistant: ' }}"
+    "{% endif %}"
+)
+
+def train(script_args, training_args):
+    train_dataset = load_dataset('json', data_files=os.path.join(script_args.dataset_path, 'train_dataset.json'), split='train')
+    test_dataset = load_dataset('json', data_files=os.path.join(script_args.dataset_path, 'test_dataset.json'), split='train')
+
+    tokenizer = AutoTokenizer.from_pretrained(script_args.model_name, use_fast=True) # Rust 기반 Fast tokenizer 사용
+    tokenizer.pad_token = tokenizer.eos_token 
+    tokenizer.chat_template = LLAMA_3_CHAT_TEMPLATE
+    tokenizer.padding_side = 'right'
+
+    def template_dataset(examples):
+        return {'text':tokenizer.apply_chat_template(examples['messages'], tokenize=False)} # 토큰화 없이, 문자열 그대로를 text 키에 매핑해서 리턴
+    
+    train_dataset = train_dataset.map(template_dataset, remove_columns=["messages"])
+    test_dataset = test_dataset.map(template_dataset, remove_columns=["messages"])
+
+    # for check
+    with training_args.main_process_first(desc='Log a few random samples from the processed training set.'):
+        for index in random.sample(range(len(train_dataset)), 2):
+            print(train_dataset[index]['text'])
+
+    model = AutoModelForCausalLM.from_pretrained(
+        script_args.model_name,
+        attn_implementation='sdpa',
+        torch_dtype=torch.bfloat16,
+        use_cache=False if training_args.gradient_checkpointing else True # gradient_checkpointing과 use_cache와 동시 사용 불가
+    )
+
+    if training_args.gradient_checkpointing:
+        model.gradient_checkpointing_enable()
+
+    # Train setting
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        dataset_text_field='text',
+        eval_dataset=test_dataset,
+        max_seq_length=script_args.max_seq_length,
+        tokenizer=tokenizer,
+        packing=True,
+        dataset_kwargs={
+            "add_special_tokens": False,
+            "append_concat_token": False,
+        },
+    )
+
+    # checkpoint
+    checkpoint=None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    
+    trainer.train(resume_from_checkpoint=checkpoint)
+
+    if trainer.is_fsdp_enabled:
+        trainer.accelerator.state.fsdp_plugin.set_state_dict_type('FULL_STATE_DICT')
+    
+    trainer.save_model()
